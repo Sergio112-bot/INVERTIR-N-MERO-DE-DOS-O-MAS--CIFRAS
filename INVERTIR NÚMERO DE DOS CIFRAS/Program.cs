ac83d8035024b9797a02e2a033f3ed0a12342458
Sergio Gonzalez <sergiogonzalezlopez101@gmail.com>
2022-03-01 23:33:21 -0600

    Agregar archivos de proyecto.

---
+++ INVERTIR NÚMERO DE DOS CIFRAS/Program.cs
@@ -0,0 +1,24 @@
+﻿    using System;
+
+namespace INVERTIR_NÚMERO_DE_DOS_CIFRAS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            int rem = 0;
+            int reverse = 0;
+            Console.WriteLine("Digite el numero para invertirlo");
+            num = int.Parse(Console.ReadLine());
+            while (num != 0)
+            {
+                rem = num % 10;
+                reverse = reverse * 10 + rem;
+                num /= 10;
+                //Console.WriteLine(rem + "\nReverse: " + reverse + "\nnum: " + num);
+            }
+            Console.WriteLine("Es: " + reverse);
+        }
+    }
+}
